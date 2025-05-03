c849a6bf00184b60e439bf45f5b7c21c2f4e555d
seyori <osho.d.joseph@gmail.com>
2025-05-03 11:41:22 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Work in progress if you find this project and can make inputs contact me via telegram t.me/s3yorii
+from setuptools import setup, find_packages
+
+setup(
+    name='mega',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Seyori',
+    description='I modified this to be able to handle large file transfers with no bandwith limit',
+    url='https://github.com/oluwaseyori/mega',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
